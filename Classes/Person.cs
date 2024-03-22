0044fb5334045e0d745d72b46919edee0e39e751
user14 <user14>
2024-03-22 14:32:42 +0300

    Добавьте файлы проекта.

---
+++ Classes/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avtorisation.Classes
+{
+    public class Person
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Post { get; set; }
+        public override string ToString()
+        {
+            return $"FullName: {FullName} Post: {Post}";
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using avtorisation;
+using System;
+using System.Windows;
+
+namespace RegistrationApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = txtLogin.Text;
+            string Password = txtPassword.Password;
+
+
+            if (!string.IsNullOrEmpty(Login) && !string.IsNullOrEmpty(Password))
+            {
+                 if ((txtLogin.Text == "TheMostSuccsesfullProgrammerInTheWorld") && (txtPassword.Password == "123Cool"))
+                 {
+                     //MessageBox.Show("Registration successful!");
+                     Window1 window1 = new Window1();
+                     window1.Show();
+                     Close();
+                 }
+
+                 if ((txtLogin.Text == "TheQueenOfClean") && (txtPassword.Password == "BestCleaner001"))
+                 {
+                     //MessageBox.Show("Registration successful!");
+                    Window1 window1 = new Window1();
+                    window1.Show();
+                    Close();
+
+                }
+                 else
+                 {
+                     MessageBox.Show("Please enter both username and password.");
+                 }
+            }
+            else
+            {
+                MessageBox.Show("Please enter both username and password.");
+            }
+            string name = txtLogin.Text != string.Empty ? txtLogin.Text:string.Empty;
+
+
+        }
+    }
+}
